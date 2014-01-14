c1c31acf2e0d3ff6d69b6b5028320d2823358deb
polev <polevong@live.com>
2014-01-14 11:56:54 +0800

    fix

--- oauth/SinaOAuth.php
+++ oauth/SinaOAuth.php
@@ -2,30 +2,33 @@
 class SinaOAuth extends POAuth
 {	
 	function getLoginUrl($callbackurl=NULL) {
-		if (!$callbackurl) {
-			$callbackurl = self::$callback;
-		}
-		$o = new SaeTOAuthV2($this->appid, $this->key);
-		return $o->getAuthorizeURL($callbackurl);
+		$data = array(
+				'client_id'=>$this->appid, 'redirect_uri'=>self::$callback,
+				//'scope'=>'all',
+		);
+		return 'https://api.weibo.com/oauth2/authorize?'.http_build_query($data);
 	}
 	
-	function getUserInfo($request = null) {
-		if (!$request) $request = $_REQUEST;
-		$o = new SaeTOAuthV2($this->appid, $this->key);
-		$r = $o->getAccessToken('code', array('code'=>$request['code'], 'redirect_uri'=>$request['callbackurl']));
+	function getUserInfo($request=NULL) {
+		$data = array(
+				'grant_type'=>'authorization_code', 'client_id'=>$this->appid, 'client_secret'=>$this->appkey,
+				'code'=>$request['code'], 'redirect_uri'=>self::$callback,
+		);
+		$r = PHttp::post('https://api.weibo.com/oauth2/access_token', $data);
+		$r = json_decode($r, 1);
 		$this->access_token = $r['access_token'];
-		$c = new SaeTClientV2($this->appid, $this->key, $r['access_token']);
-		$user = $c->get_uid();
-		$this->openid = $user['uid'];
-		$rinfo = $c->show_user_by_id($user['uid']);
+		$this->openid = $r['uid'];
+		$rinfo = PHttp::get('https://api.weibo.com/2/users/show.json', array('access_token'=>$this->access_token, 'uid'=>$this->openid));
+		$rinfo = json_decode($rinfo, 1);
 		return array(
 				'access_token'=>$this->access_token, 'expires_in'=>$r['expires_in'],
-				'uid'=>$this->openid, 'name'=>$rinfo['name'], 'avatar'=>$rinfo['avatar_large'], 
+				'uid'=>$this->openid, 'name'=>$rinfo['screen_name'], 'avatar'=>$rinfo['avatar_large'],
 				'refresh_token'=>''
 		);
 	}
 	
 	function add_t($content, $img = null, $pos = array(), $args = array()) {
+		// @todo add_t
 		$c = new SaeTClientV2($this->appid, $this->key, $this->access_token);
 		if ($img) $r = $c->upload($content, $img, $pos['wei'], $pos['jing']);
 		else $r = $c->update($content, $pos['wei'], $pos['jing']);
@@ -33,6 +36,7 @@ class SinaOAuth extends POAuth
 	}
 	
 	function follow($rid) {
+		// @todo follow
 		$c = new SaeTClientV2($this->appid, $this->key, $this->access_token);
 		return $c->follow_by_id($rid);
 	}
