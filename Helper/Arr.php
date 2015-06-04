40203dba3112837491cc7567feb5e8d46c1a8820
polev <i@poper.wang>
2015-06-04 15:35:38 +0800

    Arr::merge

--- Helper/Arr.php
+++ Helper/Arr.php
@@ -8,4 +8,13 @@ class Arr extends IlluminateArr
     {
         return array_sum(self::fetch($array, $key));
     }
+
+    static function merge($array1, $array2)
+    {
+        $array2 = Arr::dot($array2);
+        foreach ($array2 as $k => $v) {
+            Arr::set($array1, $k, $v);
+        }
+        return $array1;
+    }
 }
