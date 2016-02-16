38bf6140b07290565a0e4dadbb1597ed39883925
王波 <i@boof.wang>
2016-02-16 15:31:17 +0800

    add cookie in curl

--- Http/Client.php
+++ Http/Client.php
@@ -2,13 +2,17 @@
 
 class Client
 {
-    private static function curl($method, $url, $data = [], $header = [])
+    private static function curl($method, $url, $data = [], $header = [], $cookie = '')
     {
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
         if ($header) {
             curl_setopt($ch, CURLOPT_HTTPHEADER , $header);
+        }
+
+        if ($cookie) {
+            curl_setopt($ch, CURLOPT_COOKIE , $cookie);
         }
 
         if ($method === 'POST') {
@@ -23,34 +27,42 @@ class Client
         }
 
         $ret =  curl_exec($ch);
+
+        $chinfo = curl_getinfo($ch);
+        while ($chinfo['redirect_url']) {
+            curl_setopt($ch, CURLOPT_URL, $chinfo['redirect_url']);
+            $ret = curl_exec($ch);
+            $chinfo = curl_getinfo($ch);
+        }
+
         curl_close($ch);
 
         return $ret;
     }
 
-    static function get($url, $data = [], $header = [])
+    static function get($url, $data = [], $header = [], $cookie = '')
     {
         if ($data) {
             $url .= '?'.http_build_query($data);
         }
-        return self::curl('GET', $url, [], $header);
+        return self::curl('GET', $url, [], $header, $cookie);
     }
 
-    static function post($url, $data = [], $header = [])
+    static function post($url, $data = [], $header = [], $cookie = '')
     {
-        return self::curl('POST', $url, $data, $header);
+        return self::curl('POST', $url, $data, $header, $cookie);
     }
 
-    static function delete($url, $data = [], $header = [])
+    static function delete($url, $data = [], $header = [], $cookie = '')
     {
-        return self::curl('DELETE', $url, $data, $header);
+        return self::curl('DELETE', $url, $data, $header, $cookie);
     }
 
-    static function raw($url, $raw = '', $args = [], $header = [])
+    static function raw($url, $raw = '', $args = [], $header = [], $cookie = '')
     {
         if ($args) {
             $url .= '?'.http_build_query($args);
         }
-        return self::curl('POSTRAW', $url, $raw, $header);
+        return self::curl('POSTRAW', $url, $raw, $header, $cookie);
     }
 }
